816696adff5767ce1e9cf93f18e48e67d921f918
Jake Watkins <jwatkins@mozilla.com>
2019-09-29 11:30:49 -0700

    Fix docker profile

--- modules/roles_profiles/manifests/profiles/docker.pp
+++ modules/roles_profiles/manifests/profiles/docker.pp
@@ -2,6 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class { 'docker':
-  version => 'latest',
+class roles_profiles::profiles::docker {
+    # Install docker to latest version
+    class { 'docker':
+        version => 'latest',
+    }
 }
